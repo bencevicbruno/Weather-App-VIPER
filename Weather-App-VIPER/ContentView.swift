81c3f61da4b9d590ed146c64388b4edeb0376b7b
Bruno Benčević <bencevic.bruno@gmail.com>
2021-10-10 16:55:15 +0200

    First commut

---
+++ Weather-App-VIPER/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Weather-App-VIPER
+//
+//  Created by Bruno Benčević on 10/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
